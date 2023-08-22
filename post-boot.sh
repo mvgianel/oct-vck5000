822df11c08169ce4f3439908638364dcc975818c
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-08-22 02:57:27 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -1,2 +1,2 @@
 echo "Downloading XRT"
-wget https://raw.githubusercontent.com/Xilinx/Vitis-AI/master/board_setup/v70/scripts/install_xrt.sh
+wget -cO - "https://raw.githubusercontent.com/Xilinx/Vitis-AI/master/board_setup/v70/scripts/install_xrt.sh" > /tmp/install_xrt.sh
