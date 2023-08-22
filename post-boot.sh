85f1774eaa586c6b7fafdc42cd918da66bef68e6
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-08-22 05:19:10 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -1,4 +1,13 @@
-echo "Downloading XRT"
+echo "Install docker"
+apt update 
+apt install -y apt-transport-https ca-certificates curl software-properties-common 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" 
+apt-cache policy docker-ce 
+apt install -y docker-ce 
+usermod -aG docker ${USER}
+
+echo "Download V70 scripts"
 mkdir -p /local/repository/scripts
 wget -cO - "https://raw.githubusercontent.com/Xilinx/Vitis-AI/master/board_setup/v70/scripts/install_xrt.sh" > /local/repository/scripts/install_xrt.sh
 wget -cO - "https://raw.githubusercontent.com/Xilinx/Vitis-AI/master/board_setup/v70/scripts/install_xrm.sh" > /local/repository/scripts/install_xrm.sh
