2f1ea9f93ea20e52d3e0efb1f46465a6cd7ecc69
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-08-22 05:10:26 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -8,3 +8,13 @@ wget -cO - "https://raw.githubusercontent.com/Xilinx/Vitis-AI/master/board_setup
 apt update
 cd /local/repository
 bash ./install.sh
+
+# Install V70 card platform
+wget -cO - "https://www.xilinx.com/bin/public/openDownload?filename=xilinx-v70-gen5x8-qdma-base_2-20221028_all.deb.tar.gz" > /tmp/shell.tgz
+tar xzvf /tmp/shell.tgz -C /tmp/
+apt-get install -y /tmp/xilinx*
+
+# Install the DPU xclbin
+wget -cO - "https://www.xilinx.com/bin/public/openDownload?filename=DPUCV2DX8G_xclbins_3_0_0.tar.gz" > /tmp/xclbins.tar.gz
+tar -xzf /tmp/xclbins.tar.gz --directory /
+rm /tmp/xclbins.tar.gz
