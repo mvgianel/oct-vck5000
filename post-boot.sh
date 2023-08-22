557fa9d91eb2f6c80872cb644d1e9e17ecdd0165
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-08-22 03:31:24 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -1,4 +1,5 @@
 echo "Downloading XRT"
+mkdir -p /local/repository/scripts
 wget -cO - "https://raw.githubusercontent.com/Xilinx/Vitis-AI/master/board_setup/v70/scripts/install_xrt.sh" > /local/repository/scripts/install_xrt.sh
 wget -cO - "https://raw.githubusercontent.com/Xilinx/Vitis-AI/master/board_setup/v70/scripts/install_xrm.sh" > /local/repository/scripts/install_xrm.sh
 wget -cO - "https://raw.githubusercontent.com/Xilinx/Vitis-AI/master/board_setup/v70/scripts/install_v70_xclbins.sh" > /local/repository/scripts/install_v70_xclbins.sh
