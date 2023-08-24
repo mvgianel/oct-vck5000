d34710025598d5b1028912ad4f2e696787b24b6c
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-08-24 17:08:34 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -5,28 +5,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" 
 apt-cache policy docker-ce 
 apt install -y docker-ce 
-usermod -aG docker ${USER}
 
-REPO_URL="https://github.com/OCT-FPGA/Vitis-AI"
-
-bash -c 'cd /local/repository && git clone "$REPO_URL" && cd Vitis-AI/board_setup/v70 && source install.sh'
-
-#mkdir /docker 
-#/usr/local/etc/emulab/mkextrafs.pl /docker 
-
-# Specify the desired data directory
-new_data_path="/docker"
-
-# Create the daemon.json file with the specified content
-echo '{
-  "data-root": "'"$new_data_path"'"
-}' | sudo tee /etc/docker/daemon.json > /dev/null
-
-# Restart Docker
-sudo systemctl restart docker
-echo "Docker data directory updated to $new_data_path"
-
-#!/bin/sh
 set -x
 
 SCRIPTNAME=$0
@@ -41,6 +20,20 @@ sudo -u $GENIUSER $SCRIPTNAME
 exit $?
 fi
 HOMEDIR="/users/$USER"
-cd "$HOMEDIR" || exit
-git clone "$REPO_URL"
 
+bash -c "cd '$HOMEDIR' || exit; git clone '$REPO_URL'; cd Vitis-AI/board_setup/v70; source install.sh"
+
+usermod -aG docker ${USER}
+newgrp docker
+
+# Specify the desired data directory
+new_data_path="/docker"
+
+# Create the daemon.json file with the specified content
+echo '{
+  "data-root": "'"$new_data_path"'"
+}' | sudo tee /etc/docker/daemon.json > /dev/null
+
+# Restart Docker
+sudo systemctl restart docker
+echo "Docker data directory updated to $new_data_path"
