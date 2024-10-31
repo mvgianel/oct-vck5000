43bd7ea0a97aac82bee1826b7107c1ceb5a9fb5e
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2024-10-30 23:22:27 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -77,9 +77,9 @@ for nodeName in nodeList:
     host.disk_image = params.osImage
 
     if params.workflow == 'Vitis':
-        host.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-boot.sh " + params.toolVersion + "  >> /local/logs/output_log.txt"))
+        host.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-boot-vitis.sh " + params.toolVersion + "  >> /local/logs/output_log.txt"))
     elif params.workflow == 'Vitis-AI':  
-        host.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-boot-vitis-ai.sh " + params.dockerImage + " >> /local/repository/output_log.txt"))
+        host.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-boot-vitis-ai.sh " + params.dockerImage + " >> /local/logs/output_log.txt"))
     # Since we want to create network links to the FPGA, it has its own identity.
     #fpga = request.RawPC("fpga-" + nodeName)
     # UMass cluster
